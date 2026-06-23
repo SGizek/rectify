cd824a8e5137ac972cbd5c695a78d433eee8bc9b
RubyOnWails <wutatf@protonmail.com>
2026-06-23 16:50:47 -0400

    feat: add electron wrapper

---
+++ electron/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 800,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadURL("https://rectify-omega.vercel.app");
+  win.setMenuBarVisibility(false);
+});
+
+app.on("window-all-closed", () => app.quit());
